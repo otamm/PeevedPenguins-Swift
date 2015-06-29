1ab92ddfb77874a6ff74d896836e98293e821263
otamm <oivatom@gmail.com>
2015-06-28 23:04:35 -0400

    Added 'Gameplay' layer

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -4,8 +4,14 @@ class MainScene: CCNode {
     
     /* cocos2d methods */
     
+    // automatically executes when scene is loaded.
     func didLoadFromCCB() {
         CCBReader.load("Penguin.ccb");
         CCBReader.load("Seal.ccb");
+    }
+    
+    // triggered when button with selector 'play' is pressed
+    func play() {
+        println("Play button pressed");
     }
 }
