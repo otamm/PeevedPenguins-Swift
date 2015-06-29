cfd6ac432fac0d4cbce0a3a437701690cb4efa40
otamm <oivatom@gmail.com>
2015-06-28 21:25:59 -0400

    Added sprites and classes for seal and penguin'

---
+++ Penguin.swift
@@ -0,0 +1,13 @@
+//
+//  Penguin.swift
+//  PeevedPenguins
+//
+//  Created by Otavio Monteagudo on 6/28/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Penguin: CCSprite {
+   
+}
---
+++ Seal.swift
@@ -0,0 +1,13 @@
+//
+//  Seal.swift
+//  PeevedPenguins
+//
+//  Created by Otavio Monteagudo on 6/28/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Seal: CCSprite {
+   
+}
